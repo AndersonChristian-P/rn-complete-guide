a5bb6e9a7d3f2f60f5215e266cfaa400f0f11cc6
Christian Anderson <ca96187@gmail.com>
2019-09-29 14:32:51 -0600

    split out Goal input and output into their own components

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
+import GoalItem from "./components/GoalItem/GoalItem"
+import GoalInput from "./components/GoalInput/GoalInput"
+
 export default function App() {
 
   // --STATE-- //
-  const [enteredGoal, setEnteredGoal] = useState("")
   const [courseGoals, setCourseGoals] = useState([])
 
   // --METHODS-- //
-  const goalInputHandler = enteredText => {
-    setEnteredGoal(enteredText)
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = enteredGoal => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { key: Math.random().toString(), value: enteredGoal }
@@ -22,26 +20,12 @@ export default function App() {
   // -- -- //
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button
-          title="ADD"
-          onPress={addGoalHandler}
-        />
-      </View>
+
+      <GoalInput onAddGoal={addGoalHandler} />
 
       <FlatList
         data={courseGoals}
-        renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-          </View>
-        )}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
       />
 
     </View >
@@ -51,27 +35,9 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: 200,
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
   }
 })
 
 
 
-// when you have an unknown amount of items in a list that could be rendered you would use FlatList. FlatList takes in data. The data should point to an array. The other property is a a renderItem. renderItem takes a function that is called for every item in your data to render a list item
+// when you have an unknown amount of items in a list that could be rendered you would use FlatList. FlatList takes in data - the data should point to an array. The other property is a a renderItem. renderItem takes a function that is called for every item in your data to render a list item
